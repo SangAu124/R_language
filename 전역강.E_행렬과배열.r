324cf11442b394a4a74b9f234a16076e1c0d4343
unknown <81676542+SangAu124@users.noreply.github.com>
2021-07-05 13:38:16 +0900

    전역강.r
    전역강.r

--- 전역강.E_행렬과배열.r
+++ 전역강.E_행렬과배열.r
@@ -6,7 +6,8 @@
 #        변수명을 벡터값이 와야 함...
 
 x <- c(1, 2, 3, 4, 5, 6)
-matrix(x, nrow = 2, ncol = 3)
+matrix(x, nrow = 2, ncol = 3) # 변수 x를 2 * 3 행렬로 구성
+matrix(x, nrow = 3, ncol = 2) # 변수 x를 3 * 2 행렬로 구성
 
 
 
