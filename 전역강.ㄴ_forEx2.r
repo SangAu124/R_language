c3f49a088d5ecf472ba806ae2cef000a80b6331b
unknown <81676542+SangAu124@users.noreply.github.com>
2021-07-06 16:18:18 +0900

    전역강.r
    전역강.r

--- 전역강.ㄴ_forEx2.r
+++ 전역강.ㄴ_forEx2.r
@@ -8,3 +8,17 @@ for1 <- function(x){
 }
 
 for1(3)
+
+# 문제 2. 사용자에게 숫자를 입력받아 1부터 입력받은 숫자까지의 합계값 출력
+
+for2 <- function(x){
+  add <- 0
+  for(i in 1:x){
+    add = add + i
+  }
+  add
+}
+
+for2(10)
+for2(100)
+
